27e85b50de08c45512a1b43fbdcc9d53daae19f5
mihalw28 <michal.waszak@yahoo.com>
2019-05-19 22:52:09 +0200

    Add changes to style using black, sort imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,19 @@
 from flask import Flask, session
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from flask_bootstrap import Bootstrap
 
 from config import Config
 
-
-application = app  = Flask(__name__)
+application = app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 csrf = CSRFProtect(app)
 bootstrap = Bootstrap(app)
 
-from app import routes, models
-
+from app import models, routes
 
 if __name__ == "__main__":
     application.run(debug=False)
-
