6e1382919803f05512de2b3f519ec8d862bcc14c
mihalw28 <michal.waszak@yahoo.com>
2019-05-19 19:28:00 +0200

    Change db from SQLite to PostgreSQL

--- config.py
+++ config.py
@@ -8,6 +8,13 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 class Config(object):
     SECRET_KEY = os.environ["SECRET_KEY"] or "that-is-really-strong-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    PSQL_USER = os.environ["PSQL_USER"]
+    PSQL_PW = os.environ["PSQL_PW"]
+    DB_NAME = os.environ["DB_NAME"]
+    PSQL_HOST = os.environ["PSQL_HOST"]
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{user}:{pw}@{host}:5432/{db}".format(
+        user=PSQL_USER, pw=PSQL_PW, host=PSQL_HOST, db=DB_NAME
+    ) 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOKS_PER_PAGE = 5
