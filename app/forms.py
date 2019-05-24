a80bd77ce0477ac853b7ecac25933d85b3c2b5bd
mihalw28 <michal.waszak@yahoo.com>
2019-05-24 14:20:08 +0200

    Add book validator based on title to AddBook form.

--- app/forms.py
+++ app/forms.py
@@ -20,6 +20,14 @@ class AddBook(FlaskForm):
     description = TextAreaField("Opis", validators=[DataRequired()])
     submit = SubmitField("Dodaj do biblioteki!")
 
+    def validate_title(self, title):
+        """Basic validator. Assumption/simplification: two books with one title do not exist."""
+        # In a commercial project a better solution will be making validation based on
+        # books' ISBN number.
+        book = Book.query.filter_by(title=self.title.data).first()
+        if book is not None:
+            raise ValidationError("Książka o takim tytule już znajduje się w bazie.")
+
 
 class ImportBooks(FlaskForm):
     intitle = StringField("Tytuł książki")
